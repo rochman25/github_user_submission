03118f1e6d14a52b878885bf746e13623e2a7f49
rochman25 <zaenur.rochman98@gmail.com>
2021-11-11 13:37:36 +0700

    add search listener

--- app/src/main/java/com/example/githubusersubmission/MainActivity.kt
+++ app/src/main/java/com/example/githubusersubmission/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.githubusersubmission
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubusersubmission.databinding.ActivityMainBinding
 
@@ -24,6 +30,27 @@ class MainActivity : AppCompatActivity() {
         listGithubUser.addAll(listGithubUsers)
         showRecyclerList()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.search)?.actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
     }
 
     private val listGithubUsers: ArrayList<GithubUser>
